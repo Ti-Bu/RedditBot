7da4f2681206546f304524644f2a26eb4f9d7af0
Ti-Bu <82081478+Ti-Bu@users.noreply.github.com>
2023-07-22 09:51:36 +0200

    Update Vegan Template.user.js

--- Vegan Template.user.js
+++ Vegan Template.user.js
@@ -2,7 +2,7 @@
 // @name         Vegan Template
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the canvas!
+// @description  none
 // @author       TB
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
